74424f47350d37ad81922dc45d76d52112d2a1e0
andndre <agungandre687@gmail.com>
2024-03-09 21:50:15 +0800

    feat: form field with icons

--- src/lib/components/custom/auth-layout/LoginForm.svelte
+++ src/lib/components/custom/auth-layout/LoginForm.svelte
@@ -1,39 +1,38 @@
 <script lang="ts">
-  import * as Form from "$lib/components/ui/form";
-  import { Input } from "$lib/components/ui/input";
-  import { formSchema, type FormSchema } from "./loginSchema";
-  import {
-    type SuperValidated,
-    type Infer,
-    superForm,
-  } from "sveltekit-superforms";
-  import { zodClient } from "sveltekit-superforms/adapters";
- 
-  export let data: SuperValidated<Infer<FormSchema>>;
- 
-  const form = superForm(data, {
-    validators: zodClient(formSchema),
-  });
- 
-  const { form: formData, enhance } = form;
+	import * as Form from '$lib/components/ui/form';
+	import { Input } from '$lib/components/ui/input';
+	import { LockIcon, UserIcon } from 'lucide-svelte';
+	import { formSchema, type FormSchema } from './loginSchema';
+	import { type SuperValidated, type Infer, superForm } from 'sveltekit-superforms';
+	import { zodClient } from 'sveltekit-superforms/adapters';
+	import FormFieldIcons from '../form/form-field-icons.svelte';
+
+	export let data: SuperValidated<Infer<FormSchema>>;
+
+	const form = superForm(data, {
+		validators: zodClient(formSchema)
+	});
+
+	const { form: formData, enhance } = form;
 </script>
 
 <form method="POST" class="space-y-6" use:enhance>
-  <Form.Field {form} name="username">
-    <Form.Control let:attrs>
-      <Form.Label>Username</Form.Label>
-      <Input {...attrs} bind:value={$formData.username} />
-    </Form.Control>
-    <Form.FieldErrors />
-  </Form.Field>
-  <Form.Field {form} name="password">
-    <Form.Control let:attrs>
-      <Form.Label>Password</Form.Label>
-      <Input {...attrs} bind:value={$formData.password} />
-    </Form.Control>
-    <Form.FieldErrors />
-  </Form.Field>
-  <Form.Button class="w-full">Sign In</Form.Button>
+	<FormFieldIcons
+		{form}
+		name="username"
+		placeholder="Username"
+		icon={UserIcon}
+		bind:value={$formData.username}
+	/>
+	<FormFieldIcons
+		{form}
+		name="password"
+		placeholder="Password"
+		icon={LockIcon}
+		type="password"
+		bind:value={$formData.password}
+	/>
+	<Form.Button class="w-full">Sign In</Form.Button>
 </form>
 
 <p class="mt-10 text-center text-sm text-gray-500">
---
+++ src/lib/components/custom/form/form-field-icons.svelte
@@ -0,0 +1,44 @@
+<script lang="ts" context="module">
+	import type { FormPath, SuperForm } from 'sveltekit-superforms';
+	type T = Record<string, unknown>;
+	type U = unknown;
+</script>
+
+<script lang="ts" generics="T extends Record<string, unknown>, U extends FormPath<T>">
+	import * as Form from '$lib/components/ui/form';
+	import { Input } from '$lib/components/ui/input';
+	import { EyeIcon, EyeOffIcon, type Icon } from 'lucide-svelte';
+	import type { ComponentType } from 'svelte';
+
+	export let form: SuperForm<T>;
+	export let name: U;
+	export let placeholder: string;
+	export let icon: ComponentType<Icon>;
+	export let type: 'text' | 'password' = 'text';
+	export let value: string | null = null;
+
+	let isPassword = type === 'password';
+	let showPassword = false;
+</script>
+
+<Form.Field {form} {name}>
+	<Form.Control let:attrs>
+		<div class="relative">
+			{#if isPassword}
+				<Input {...attrs} {value} {placeholder} type={showPassword ? 'text' : 'password'} class="pl-10" />
+				<svelte:component this={icon} size={16} class="absolute left-3 top-1/2 -translate-y-1/2" />
+				<button type="button" class="absolute right-3 top-1/2 -translate-y-1/2" on:click={() => (showPassword = !showPassword)}>
+					{#if showPassword}
+						<EyeOffIcon size={16}/>
+					{:else}
+						 <EyeIcon size={16}/>
+					{/if}
+				</button>
+			{:else}
+				<Input {...attrs} {value} {placeholder} {type} class="pl-10" />
+				<svelte:component this={icon} size={16} class="absolute left-3 top-1/2 -translate-y-1/2" />
+			{/if}
+		</div>
+	</Form.Control>
+	<Form.FieldErrors />
+</Form.Field>
