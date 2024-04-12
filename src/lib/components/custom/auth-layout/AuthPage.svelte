3f36325e28ff07870dc91b60ce523c2d9eec14ed
andndre <agungandre687@gmail.com>
2024-04-12 09:45:13 +0800

    update login page

--- src/lib/components/custom/auth-layout/AuthPage.svelte
+++ src/lib/components/custom/auth-layout/AuthPage.svelte
@@ -1,15 +1,20 @@
 <script lang="ts">
   import logo from "$lib/images/logo.png";
+  import introImg from "$lib/images/intro.svg";
 	export let title: string;
 </script>
 
-<div class="flex min-h-screen flex-col justify-center px-6 py-12 lg:px-8">
-  <div class="sm:mx-auto sm:w-full sm:max-w-sm">
-    <img class="mx-auto h-20 w-auto" src="{logo}" alt="Vedanta Logo">
-    <h2 class="mt-10 text-center text-2xl font-bold leading-9 tracking-tight text-gray-900">{title}</h2>
+<div class="flex">
+  <div class="flex min-h-screen flex-col justify-center px-6 py-12 lg:px-8 flex-1">
+    <div class="sm:mx-auto sm:w-full sm:max-w-sm">
+      <img class="mx-auto h-20 w-auto" src="{logo}" alt="Vedanta Logo">
+      <h2 class="mt-10 text-center text-2xl font-bold leading-9 tracking-tight text-gray-900">{title}</h2>
+    </div>
+    <div class="mt-10 sm:mx-auto sm:w-full sm:max-w-sm">
+      <slot></slot>
+    </div>
   </div>
-
-  <div class="mt-10 sm:mx-auto sm:w-full sm:max-w-sm">
-		<slot></slot>
+  <div class="hidden lg:block flex-1 p-16">
+    <img class="h-full w-full" src="{introImg}" alt="Hello!">
   </div>
 </div>
