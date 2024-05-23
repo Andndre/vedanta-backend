ae648b66d3e3b22ae13c408cd5af140e11517bf3
GusYudhi <gusyudhip@gmail.com>
2024-05-23 17:46:30 +0800

    kelas

--- src/routes/api/class/+server.ts
+++ src/routes/api/class/+server.ts
@@ -11,11 +11,32 @@ export const GET = async (evt) => {
 	const classes = await prismaClient.userKelas.findMany({
 		where: {
 			userId: user.id
+		},
+		select: {
+			kelas: {
+				select: {
+					id: true,
+					name: true,
+					pengajar: {
+						select: {
+							name: true
+						}
+					}
+				}
+			}
 		}
 	});
+
+	const response = classes.map((c) => ({
+		className: c.kelas.name,
+		teacherName: c.kelas.pengajar.name,
+		id: c.kelas.id
+	}));
+
 	// cache for 5 minutes
 	evt.setHeaders({
 		'Cache-Control': 'public, max-age=300'
 	});
-	return json({ classes, error: false });
+
+	return json({ classes: response, error: false });
 };
