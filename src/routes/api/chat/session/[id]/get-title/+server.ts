6fb861c574c4220f28a9a70bd666d00e22999778
GusYudhi <gusyudhip@gmail.com>
2024-05-24 22:34:58 +0800

    cleanup

--- src/routes/api/chat/session/[id]/get-title/+server.ts
+++ src/routes/api/chat/session/[id]/get-title/+server.ts
@@ -35,30 +35,3 @@ export const GET = async (evt) => {
 		return error(500, 'Something went wrong');
 	}
 };
-
-// // import { Endpoint, z, error, type RouteModifier } from 'sveltekit-api';
-
-// // export const Param = z.object({
-// // 	id: z.string()
-// // });
-
-// // export const Output = z.object({
-// // 	title: z.string()
-// // });
-
-// // export const Error = {
-// // 	500: error(500, 'Something went wrong'),
-// // 	404: error(404, 'Session not found')
-// // };
-
-// // export const Modifier: RouteModifier = (r) => {
-// // 	r.tags = ['Chat'];
-// // 	r.summary = 'Get Chat Title';
-// // 	r.description = 'Dapatkan informasi judul dari chat session dengan id tertentu';
-// // 	return r;
-// // };
-
-// // // @ts-ignore
-// // export default new Endpoint({ Modifier, Output, Error, Param }).handle(async () => {
-// // 	return new Response();
-// // });
