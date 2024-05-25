77a46c1aecf9fe006be2991461638d40ddcd1bcb
GusYudhi <gusyudhip@gmail.com>
2024-05-25 19:45:32 +0800

    doa cache for 5 minutes

--- src/routes/dashboard/admin/doa/+page.server.ts
+++ src/routes/dashboard/admin/doa/+page.server.ts
@@ -4,5 +4,10 @@ import type { PageServerLoad } from './$types';
 export const load: PageServerLoad = async (evt) => {
 	const doas = await prismaClient.doa.findMany({});
 
+	// set cache
+	evt.setHeaders({
+		'Cache-Control': 'public, max-age=300'
+	});
+
 	return { doas };
 };
