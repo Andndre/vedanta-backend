74131df8f265eba4c71c8b64e488169a702aae36
Andndre <agungandre687@gmail.com>
2024-05-28 21:02:06 +0800

    favorites sloka

---
+++ src/routes/api/gita/favorites/+server.ts
@@ -0,0 +1,46 @@
+import { prismaClient } from '@/db.js';
+import { json } from '@sveltejs/kit';
+
+export const GET = async (evt) => {
+	const user = evt.locals.apiUser!;
+
+	const favorites = await prismaClient.userLikedGitaSloka.findMany({
+		where: {
+			userId: user.id
+		},
+		select: {
+			sloka: {
+				select: {
+					number: true,
+					id: true,
+					numberBab: true,
+					content: true,
+					translationIndo: true,
+					usersLiked: {
+						where: {
+							userId: user.id
+						},
+						select: {
+							id: true
+						}
+					}
+				}
+			}
+		}
+	});
+
+	// Check if usersLiked contains any items and set isLiked accordingly
+	const withIsLiked = favorites.map((sloka) => {
+		// Destructure usersLiked and create a new object without it
+		const res = { ...sloka, isLiked: sloka.sloka.usersLiked.length > 0 };
+		return {
+			id: res.sloka.id,
+			isLiked: res.isLiked,
+			number: res.sloka.number,
+			content: res.sloka.content,
+			translationIndo: res.sloka.translationIndo
+		};
+	});
+
+	return json({ slokas: withIsLiked });
+};
