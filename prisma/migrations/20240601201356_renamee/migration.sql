2c3b42cbe51ffcdd0a708139029a770d0af78e81
Andndre <agungandre687@gmail.com>
2024-06-02 04:26:39 +0800

    argh

--- prisma/migrations/20240601201356_renamee/migration.sql
+++ prisma/migrations/20240601201356_renamee/migration.sql
@@ -1,12 +1,8 @@
 /*
   Warnings:
 
-  - A unique constraint covering the columns `[userId,quizId]` on the table `UserAnswerQuizEntry` will be added. If there are existing duplicate values, this will fail.
   - A unique constraint covering the columns `[userId,quizId]` on the table `UserQuizResult` will be added. If there are existing duplicate values, this will fail.
 
 */
--- CreateIndex
-CREATE UNIQUE INDEX `UserAnswerQuizEntry_userId_quizId_key` ON `UserAnswerQuizEntry`(`userId`, `quizId`);
-
 -- CreateIndex
 CREATE UNIQUE INDEX `UserQuizResult_userId_quizId_key` ON `UserQuizResult`(`userId`, `quizId`);
