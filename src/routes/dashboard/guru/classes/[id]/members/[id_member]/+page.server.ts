5c6244c63fc823822d95f7b5d9e0a4b227cdd86f
manikmahardikaa <127854755+manikmahardikaa@users.noreply.github.com>
2024-06-10 09:23:03 +0700

    grafik siswa

---
+++ src/routes/dashboard/guru/classes/[id]/members/[id_member]/+page.server.ts
@@ -0,0 +1,19 @@
+import { prismaClient } from '@/db';
+import type { PageServerLoad } from './$types';
+import { error } from '@sveltejs/kit';
+
+export const load: PageServerLoad = async (evt) => {
+	const user = await prismaClient.user.findUnique({
+		where: {
+			id: evt.params.id_member
+		}
+	});
+
+	if (!user) {
+		throw error(404, 'User not found');
+	}
+
+	return {
+		user
+	};
+};
--- src/routes/dashboard/guru/classes/[id]/members/[id_member]/+page.svelte
+++ src/routes/dashboard/guru/classes/[id]/members/[id_member]/+page.svelte
@@ -1,9 +1,69 @@
 <script lang="ts">
-	import Radar from 'svelte-chartjs/src/Radar';
+	import { onMount } from 'svelte';
+	import Chart from 'chart.js/auto';
+	import type { PageServerData } from './$types';
+
+	export let data: PageServerData;
+
+	let portfolio: HTMLCanvasElement;
+	const chartData = {
+		labels: [
+			'Quiz Diselesaikan',
+			'Pertanyaan Ganesh Bot',
+			'Bertanya pada Diskusi',
+			'Menjawab pada Diskusi'
+		],
+		datasets: [
+			{
+				label: 'Data Siswa',
+				data: [
+					data.user.quizCompleted,
+					data.user.ganeshBotMessages,
+					data.user.discussionsAsked,
+					data.user.discussionsAnswered
+				],
+				backgroundColor: ['#7000e1', '#fc8800', '#00b0e8'],
+				// hoverOffset: 4,
+				borderWidth: 0
+			}
+		]
+	};
+
+	onMount(() => {
+		const ctx = portfolio.getContext('2d') as CanvasRenderingContext2D;
+		// Initialize chart using default config set
+		new Chart(ctx, {
+			type: 'radar',
+			data: chartData,
+			options: {
+				responsive: true,
+				plugins: {
+					legend: {
+						position: 'bottom',
+						display: true,
+						labels: {
+							usePointStyle: true,
+							padding: 20,
+							font: {
+								size: 14
+							}
+						}
+					},
+					title: {
+						display: true,
+						text: 'Informasi siswa'
+					}
+				}
+			}
+		});
+	});
 </script>
 
 <h1 class="text-3xl font-bold">Informasi Siswa</h1>
 
 <div class="pt-8"></div>
-
-<div class="rounded-sm bg-card p-6 shadow-md"></div>
+<div class="rounded-sm bg-card p-6 shadow-md">
+	<div class="aspect-square w-full max-w-3xl">
+		<canvas bind:this={portfolio} class="h-full w-full" />
+	</div>
+</div>
